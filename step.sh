4b63ef26a5a5a712e1a1ea2d16208b59b812fb20
JohanCollu <jcollu@freebox.fr>
2020-06-12 15:52:14 +0200

    update with config and scrip who merge

--- step.sh
+++ step.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+echo "This is the value specified for the input 'branch_source_name': ${branch_source_name}"
+echo "This is the value specified for the input 'branch_target_name': ${branch_target_name}"
+
+git checkout ${branch_target_name} 2>/dev/null || git checkout -b ${branch_target_name}
+
+git merge ${branch_source_name}
+git push origin ${branch_target_name}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #
 # --- Export Environment Variables for other Steps:
