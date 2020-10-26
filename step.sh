8a17b0218b7c5abe32e29aa63446d552a0fd3ad9
Thibault FIGHIERA <tfighiera@gmail.com>
2020-10-26 18:56:21 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -ex
+set -e
 
-echo "This is the value specified for the input 'branch_source_name': ${branch_source_name}"
-echo "This is the value specified for the input 'branch_target_name': ${branch_target_name}"
+echo "|\t\t Opening auto-merge MR for Source branche: ${branch_source_name} into target: ${branch_target_name}"
 
-git checkout ${branch_target_name} 2>/dev/null || git checkout -b ${branch_target_name}
+report_branch=report_${branch_source_name}
+git checkout -b ${report_branch}
 
-git merge origin/${branch_source_name}
-git push origin ${branch_target_name}
+title="Report ${branch_source_name} into ${branch_target_name}"
+git push --set-upstream origin ${report_branch} -o merge_request.create -o merge_request.target=${branch_target_name} -o merge_request.title=${title} -o merge_request.merge_when_pipeline_succeeds
+echo -e "|\t\t Created and pushed branch : ${report_branch}"
+echo -e "|\t\t MR is now opened and will be merge automatically if there is no conflict"
