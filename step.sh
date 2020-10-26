598884ce79a86bb391066a254802c65e4fd03c70
Thibault FIGHIERA <tfighiera@gmail.com>
2020-10-26 19:09:28 +0100

    fix title

--- step.sh
+++ step.sh
@@ -7,6 +7,6 @@ report_branch=report_${branch_source_name}
 git checkout -b ${report_branch}
 
 title="Report ${branch_source_name} into ${branch_target_name}"
-git push --set-upstream origin ${report_branch} -o merge_request.create -o merge_request.target=${branch_target_name} -o merge_request.title=${title} -o merge_request.merge_when_pipeline_succeeds
+git push --set-upstream origin ${report_branch} -o merge_request.create -o merge_request.target=${branch_target_name} -o merge_request.title="${title}" -o merge_request.merge_when_pipeline_succeeds 2>/dev/null
 echo -e "|\t\t Created and pushed branch : ${report_branch}"
 echo -e "|\t\t MR is now opened and will be merge automatically if there is no conflict"
