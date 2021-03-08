83170500c818e9c6f9c33beb7a4ddd898354e775
Thibault FIGHIERA <tfighiera@gmail.com>
2021-03-08 14:10:50 +0100

    Feat: use current branch name -  force source del

--- step.sh
+++ step.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-branch_source_name="HEAD"
+branch_source_name=$(git branch --show-current)
 
 diff=$(git diff $branch_source_name origin/$branch_target_name)
 
@@ -17,7 +17,7 @@ else
     echo -e "|\t Opening MR for ${report_branch} into ${branch_target_name}"
     
     title="Report ${branch_source_name} into ${branch_target_name}"
-    git push --set-upstream origin ${report_branch} -o merge_request.create -o merge_request.target=${branch_target_name} -o merge_request.title="${title}" -o merge_request.merge_when_pipeline_succeeds &> /dev/null
+    git push --set-upstream origin ${report_branch} -o merge_request.create -o merge_request.target=${branch_target_name} -o merge_request.title="${title}" -o merge_request.merge_when_pipeline_succeeds -o merge_request.remove_source_branch &> /dev/null
 
     echo -e "|\t MR is now opened and will be merge automatically if there is no conflict"
 fi
